1aa53aafff9030d36fc24a54072ffffbeb0e8767
Long Xin Chen <ryan.chen@everfortune.ai>
2023-05-19 13:33:07 +0800

    [add] tg bot / send message.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # neostocks-tg
 An example of a Python code for practicing web scraping.  
-neopets stock tg bot
+neopets stock + tg bot
--- neostock.py
+++ neostock.py
@@ -5,6 +5,10 @@ from bs4 import BeautifulSoup
 
 if __name__ == '__main__':
 
+    # tg bot
+    bot_token = 'your tg bot token'
+    bot_chatID = 'your tg chat id'
+
     # source
     url = 'https://neostocks.info/'
     response = requests.get(url=url)
@@ -20,5 +24,13 @@ if __name__ == '__main__':
 
     print('Ticker   Open     Curr     Change')
 
+    # message
+    message = str('Ticker   Open     Curr     Change\n')
+
     for json in json_s['summary_data']['1d']:
         print('{:5}'.format(json['ticker']) + '{:5}'.format(json['open']) + '{:5}'.format(json['curr']) + '{:5}'.format(json['change']))
+        message += "%-9s%-9s%-9s%-9s" % (json['ticker'] , format(json['open']) , format(json['curr']) , format(json['change'])) + '\n'
+
+    # send
+    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + message
+    response = requests.get(send_text)
